5a323106690ffb0393fa221ecb843299051c985d
BaeInHoo <qodlsgh0210@naver.com>
2022-10-26 20:20:20 +0900

    Sanity 연결하고 데이터 가져오기

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,14 +1,35 @@
-import { useRouter } from "next/router";
+import SanityService from "../../services/SanityService";
 
-export default function slug() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter();
-
-  const { slug } = router.query;
-
+export default function PostAll({ slug, post }) {
   return (
     <div>
-      <h1>Post : {slug}</h1>
+      <h1>Post : { slug }</h1>
     </div>
   );
+}
+
+export async function getStaticPaths() {
+  const home = await new SanityService().getHome();
+  const posts = await new SanityService().getPosts();
+
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.slug
+    }
+  }))
+
+  return {
+    paths,
+    failback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  const { slug } = params;
+
+  return {
+    props: {
+      slug,
+    },
+  };
 }
